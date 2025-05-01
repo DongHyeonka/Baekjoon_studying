630c68757599cb85f0f68a63597e674b5e27d0bc
Kang Dong Hyeon <144816658+DongHyeonka@users.noreply.github.com>
2025-05-01 17:54:07 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1764 ms, Memory: 301444 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -6,49 +6,24 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] arr;
-    public static long result;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int N = Integer.parseInt(st.nextToken());
 
-        arr = new int[N];
+        int[] arr = new int[10001];
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(bf.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
-        }        
-
-        sort(arr, 5);
+            arr[Integer.parseInt(st.nextToken())]++;
+        }
+        
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i = 0; i < N; i++) {
-            bw.write(arr[i] + "\n");
+        for(int i = 0; i < 10001; i++) {
+            for(int j = 0; j < arr[i]; j++) {
+                bw.write(i + "\n");
+            }
         }
         bw.flush();
         bw.close();
-    }
-
-    public static void sort(int[] arr, int N) {
-        int[] output = new int[arr.length];
-        int set = 1;
-        int count = 0;
-        while(count != N) {
-            int[] bucket = new int[10];
-            for(int i = 0; i < arr.length; i++) {
-                bucket[(arr[i] / set) % 10]++;
-            }
-            for(int i = 1; i < 10; i++) {
-                bucket[i] += bucket[i - 1];
-            }
-            for(int i = arr.length - 1; i >= 0; i--) {
-                output[bucket[(arr[i] / set) % 10] - 1] = arr[i];
-                bucket[(arr[i] / set) % 10]--;
-            }
-            for(int i = 0; i < arr.length; i++) {
-                arr[i] = output[i];
-            }
-            set *= 10;
-            count++;
-        }
     }
 }
