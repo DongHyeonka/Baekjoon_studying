aa82e66b57dab4644237f6485344f4b7319e40ca
Kang Dong Hyeon <144816658+DongHyeonka@users.noreply.github.com>
2025-10-06 17:18:27 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 9.87 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,46 +2,45 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        Map<String, Integer> termMap = new HashMap<>(); 
-        for(String term : terms) {
-            String[] termsDetail = term.split(" ");
-            String firstTerms = termsDetail[0];
-            int secondTerms = Integer.parseInt(termsDetail[1]);
-            termMap.put(firstTerms, secondTerms);
-        }
-        
-        int todayDays = dateToDay(today);
-        
-        int n = privacies.length;    
+        int n = privacies.length;
         
         List<Integer> answer = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            String[] parts = privacies[i].split(" ");
-            String collectDate = parts[0];
-            String termType = parts[1];
-            
-            int collectDays = dateToDay(collectDate);
-            int validMonths = termMap.get(termType);
-            int expireDays = addMonths(collectDays, validMonths);
-            
-            if(todayDays >= expireDays) {
-                answer.add(i + 1);
-            }
-            
-        }
-        return answer.stream().mapToInt(i -> i).toArray();
-    }
-    
-    private int dateToDay(String date) {
-        String[] parts = date.split("\\.");
-        int year = Integer.parseInt(parts[0]);
-        int month = Integer.parseInt(parts[1]);
-        int day = Integer.parseInt(parts[2]);
         
-        return year * 12 * 28 + (month - 1) * 28 + (day - 1);
+        long currentDay = calculateDay(today); // 일의 수를 반환하겠지?
+        
+        for(int i = 0; i < n; i++) {
+            String[] privacyParts = privacies[i].split(" "); // 일단 분리
+            String privacyDay = privacyParts[0];
+            String privacyKindTerm = privacyParts[1];
+            
+            long privacyToDay = calculateDay(privacyDay);
+            for(String term : terms) {
+                String[] termParts = term.split(" ");
+                String kindTerm = termParts[0];
+                String period = termParts[1];
+                long periodToLong = Long.parseLong(period);
+                
+                if(privacyKindTerm.equals(kindTerm)) {
+                    privacyToDay = privacyToDay + periodToLong * 28;
+                    if(currentDay >= privacyToDay) {
+                        answer.add(i + 1);
+                    }
+                }
+            }
+        }
+        
+        return answer.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
     
-    private int addMonths(int days, int months) {
-        return days + months * 28;
+    private long calculateDay(String day) {
+        String[] parts = day.split("\\."); // 이거 기억하자 .은 정규식 표현이라 분리가 안된다 [] 로 감싸주든 \\로 앞에 두든
+        long year = Long.parseLong(parts[0]);
+        long month = Long.parseLong(parts[1]);
+        long days = Long.parseLong(parts[2]);
+        
+        return year * 12 * 28 + month * 28 + days; //총 day 수를 반환함 겁나 커질 것을 대비해 long으로 반환
     }
 }
+// 시간을 전부 일로 바꿔서 계산
