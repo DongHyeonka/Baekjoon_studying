65591978bd38192e5ecd4660fda8faa17e63e9db
Kang Dong Hyeon <144816658+DongHyeonka@users.noreply.github.com>
2025-10-06 16:36:28 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 41.56 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -2,47 +2,59 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[][] edges) {
-        int[] answer = new int[4];
         
-        Map<Integer, Integer> indegree = new HashMap<>();
-        Map<Integer, Integer> outdegree = new HashMap<>();
-        
-        for(int[] edge : edges) {
-            int from = edge[0];
-            int to = edge[1];
-            outdegree.put(from, outdegree.getOrDefault(from, 0) + 1);
-            indegree.put(to, indegree.getOrDefault(to, 0) + 1);
-        }
-        
-        int createdVertex = 0;
-        int donutCount = 0;
-        int barCount = 0;
-        int eightCount = 0;
-        
-        Set<Integer> vertices = new HashSet<>(outdegree.keySet());
-        vertices.addAll(indegree.keySet());
-        
-        for(int vertex : vertices) {
-            int in = indegree.getOrDefault(vertex, 0);
-            int out = outdegree.getOrDefault(vertex, 0);
-            
-            if(in == 0 && out >= 2) {
-                createdVertex = vertex;
-            } else if(out == 0) {
-                barCount++;
-            } else if(in >= 2 && out == 2) {
-                eightCount++;
+        int maxNode = 0;
+        for(int i = 0; i < edges.length; i++) {
+            for(int j = 0; j < edges[i].length; j++) {
+                maxNode = Math.max(maxNode, edges[i][j]);
             }
         }
         
-        int totalGraphs = outdegree.getOrDefault(createdVertex, 0);
-        donutCount = totalGraphs - barCount - eightCount;
+        // 정점끼리 연결되어있는 부분을 확인해야됨 -> 들어가고 나가는걸 배열로 표시?
+        int[] inDegree = new int[maxNode + 1];
+        int[] outDegree = new int[maxNode + 1];
         
-        answer[0] = createdVertex;
-        answer[1] = donutCount;
-        answer[2] = barCount;
-        answer[3] = eightCount;
+        for(int[] edge : edges) {
+            int from = edge[0]; // 이걸 나오는 vertex로 보고
+            int to = edge[1]; // 이걸 들어오는 vertex로 봐보자
+            outDegree[from]++; // 나오는 값이 2 이상이면 시작 정점 또는 8자 그래프라고 알 수 잇음
+            inDegree[to]++; // 들어오는게 2 이상이고 이 값이 동시에 나오는 값도 2 이상이면 8자 겠지?
+        }
+        
+        // 시작 정점은 나가는게 2 이상이며 들어오는게 없는값인가..?
+        // 막대는 근데 마지막에 더이상 연결된게 없어야하는데 이 값이 즉 inner의 값이 0인 값이 있으며 들어오는 값이 1개라면 막대겠지?
+        // 팔자는 들어가는거 2이상 나가는거 2
+        // 도넛은 어떻게 알지..? -> 생성 정점에서 나가는 간선 - 막대 그래프수 - 8자를 빼면 도넛의 수
+        
+        
+        int vertexNum = 0;
+        int donut = 0;
+        int stick = 0;
+        int eight = 0;
+        
+        for(int i = 1; i <= maxNode; i++) {
+            if(outDegree[i] >= 2 && inDegree[i] == 0) {
+                vertexNum = i;
+            } else if(outDegree[i] == 0 && inDegree[i] >= 1) {
+                stick++;
+            } else if(outDegree[i] == 2 && inDegree[i] >= 2) {
+                eight++;
+            }
+        }
+        
+        if(vertexNum != 0)
+            donut = outDegree[vertexNum] - stick - eight;
+        
+        
+        int[] answer = new int[4];
+        answer[0] = vertexNum;
+        answer[1] = donut;
+        answer[2] = stick;
+        answer[3] = eight;
         
         return answer;
     }
 }
+// 엣지를 받았을 때 들어오는 개수와 나가는 개수를 보고 판단
+// 생성 정점 번호, 도넛 모양 그래프 수, 막대 모양 그래프 수, 8자 모양 그래프 수
+// 1차원 배열로 반환
